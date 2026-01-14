92cd9371ddd4edd18077702f30a5f5b9ba29c7a1
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-01-14 22:17:29 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react'; 
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css'; 
@@ -39,4 +38,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
 
