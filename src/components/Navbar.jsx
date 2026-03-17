61d098ffca66f82dc336328794d00ef7700fbf3a
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-03-17 10:53:34 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'; 
 import { Link } from 'react-router-dom';
-import '../styles/navbar.css'; 
+import '../styles/Navbar.css'; 
 import LogoImg from '../assets/images/logo.jpg';
 
 const Navbar = () => {
